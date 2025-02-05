5ee00b7c22f31ccdd16e1097977d9ad0fdee2d3d
RHS-Rabbi <rabbihossain187@gmail.com>
2025-02-05 20:22:36 +0600

    first commit

---
+++ Class-12/main.js
@@ -0,0 +1,29 @@
+var month = 'January';
+switch (month) {
+
+    case 'February':
+    console.log("February: 28 days (29 days in a leap year)");
+    case 'March':
+    console.log("March: 31 days");
+    case 'April':
+    console.log("April: 30 days");
+    case 'May':
+    console.log("May: 31 days");
+    case 'June':
+    console.log("June: 30 days");
+    case 'July':
+    console.log("July: 31 days");
+    case 'August':
+    console.log("August: 31 days");
+    case 'September':
+    console.log("September: 30 days");
+    case 'October':
+    console.log("October: 31 days");
+    case 'November':
+    console.log("November: 30 days");
+    case 'December':
+    console.log("December: 31 days");
+
+    default:
+     console.log("January: 31 days");
+}
