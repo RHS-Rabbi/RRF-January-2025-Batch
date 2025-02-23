f405b7bd3ff8788aa32d32c572cfba40c287b7a8
RHS-Rabbi <rabbihossain187@gmail.com>
2025-02-23 16:23:30 +0600

    Class-20

---
+++ Class-20/filezilla.php
@@ -0,0 +1,8 @@
+
+<?php include_once('header.php');?>
+
+<h2>File Zilla</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi deserunt dicta nihil in architecto blanditiis nisi quod earum error provident! Eum molestias deserunt, consectetur voluptatum aliquid qui magni asperiores sunt?</p>
+<a href="" class="btn">Contact us</a>
+
+<?php include_once('footer.php');?>
---
+++ Class-20/gallery.php
@@ -0,0 +1,29 @@
+
+<?php include_once('header.php');?>
+
+<h2>Gallery</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi deserunt dicta nihil in architecto blanditiis nisi quod earum error provident! Eum molestias deserunt, consectetur voluptatum aliquid qui magni asperiores sunt?</p>
+<a href="" class="btn">Contact us</a>
+<div class="gallery">
+    <div class="single-gallery">
+        <img src="https://placehold.co/300x200/000000/FFFFFF/png" alt="">
+    </div>
+    <div class="single-gallery">
+        <img src="https://placehold.co/300x200/000000/FFFFFF/png" alt="">
+    </div>
+    <div class="single-gallery">
+        <img src="https://placehold.co/300x200/000000/FFFFFF/png" alt="">
+    </div>
+    <div class="single-gallery">
+        <img src="https://placehold.co/300x200/000000/FFFFFF/png" alt="">
+    </div>
+    <div class="single-gallery">
+        <img src="https://placehold.co/300x200/000000/FFFFFF/png" alt="">
+    </div>
+    <div class="single-gallery">
+        <img src="https://placehold.co/300x200/000000/FFFFFF/png" alt="">
+    </div>
+</div>
+
+<?php include_once('footer.php');?>
+
