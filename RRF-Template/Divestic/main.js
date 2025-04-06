26de090459934edf2d93cb9f1e7c5175c6f855ae
RHS-Rabbi <rabbihossain187@gmail.com>
2025-04-06 07:54:12 +0600

    first-commit

---
+++ RRF-Template/Divestic/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.testimonial-slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      centerMode: true,
+      centerPadding: '100px',
+      dots:false,
+      arrows: false,
+      variableWidth: true,
+      autoplay: true,
+        autoplaySpeed: 3000,
+    });
+  });
