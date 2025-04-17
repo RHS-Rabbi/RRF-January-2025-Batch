2ac287567dbf9180f7976bc4c9aa93b0e7f5c1a7
RHS-Rabbi <rabbihossain187@gmail.com>
2025-04-18 00:09:34 +0600

    first-commit

---
+++ RRF-Template/Plumber-Point/main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function(){
+    $('.testimoniaal-slider').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      });
+});
