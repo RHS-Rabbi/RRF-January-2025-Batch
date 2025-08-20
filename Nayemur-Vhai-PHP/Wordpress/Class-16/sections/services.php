5774d981efa4ffd0b5578251ae2fbc26e9e328f9
RHS-Rabbi <rabbihossain187@gmail.com>
2025-08-20 21:33:44 +0600

    Class-16

---
+++ Nayemur-Vhai-PHP/Wordpress/Class-16/sections/services.php
@@ -0,0 +1,106 @@
+<section class="service-area">
+    <div class="container service-container">
+        <div class="service-title">
+            <h3>Our popular courses</h3>
+            <p>Build new skills with new trendy courses and shine for the next future career.</p>
+        </div>
+        <div class="service-carusel">
+            <div class="single-service">
+                <div class="single-service-item">
+                    <div class="service-bg service-bg1"></div>
+                    <div class="service-content">
+                        <a class="service-deatails" href="#">
+                            <h6>Basic web design</h6>
+                            <div class="service-rating">
+                                <span class="service-rating-icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="17" viewBox="0 0 18 17" fill="none">
+                                        <path d="M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z" fill="#FEA31B"/>
+                                    </svg>
+
+                                </span>
+                                4.5
+                            </div>
+                        </a>
+                        <p>Get the best course, gain knowledge and shine for your future career.</p>
+                        <div class="pricing-btn">
+                            <span class="service-pricing">$120.75</span>
+                            <a class="service-btn" href="#">Book Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="single-service">
+                <div class="single-service-item">
+                    <div class="service-bg service-bg2"></div>
+                    <div class="service-content">
+                        <a class="service-deatails" href="#">
+                            <h6>UI/UX design</h6>
+                            <div class="service-rating">
+                                <span class="service-rating-icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="17" viewBox="0 0 18 17" fill="none">
+                                        <path d="M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z" fill="#FEA31B"/>
+                                    </svg>
+
+                                </span>
+                                4.5
+                            </div>
+                        </a>
+                        <p>Get the best course, gain knowledge and shine for your future career.</p>
+                        <div class="pricing-btn">
+                            <span class="service-pricing">$120.75</span>
+                            <a class="service-btn" href="#">Book Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="single-service">
+                <div class="single-service-item">
+                    <div class="service-bg service-bg3"></div>
+                    <div class="service-content">
+                        <a class="service-deatails" href="#">
+                            <h6>Web App design</h6>
+                            <div class="service-rating">
+                                <span class="service-rating-icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="17" viewBox="0 0 18 17" fill="none">
+                                        <path d="M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z" fill="#FEA31B"/>
+                                    </svg>
+
+                                </span>
+                                4.5
+                            </div>
+                        </a>
+                        <p>Get the best course, gain knowledge and shine for your future career.</p>
+                        <div class="pricing-btn">
+                            <span class="service-pricing">$120.75</span>
+                            <a class="service-btn" href="#">Book Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="single-service">
+                <div class="single-service-item">
+                    <div class="service-bg service-bg1"></div>
+                    <div class="service-content">
+                        <a class="service-deatails" href="#">
+                            <h6>Basic web design</h6>
+                            <div class="service-rating">
+                                <span class="service-rating-icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="17" viewBox="0 0 18 17" fill="none">
+                                        <path d="M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z" fill="#FEA31B"/>
+                                    </svg>
+
+                                </span>
+                                4.5
+                            </div>
+                        </a>
+                        <p>Get the best course, gain knowledge and shine for your future career.</p>
+                        <div class="pricing-btn">
+                            <span class="service-pricing">$120.75</span>
+                            <a class="service-btn" href="#">Book Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
